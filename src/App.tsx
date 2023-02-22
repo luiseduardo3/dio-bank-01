ff604bb14b0659d8c5ce1b971f5895de9cf166b9
Luis Eduardo <lepereira47@gmail.com>
2023-02-22 20:05:39 -0300

    git push

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import { ChakraProvider, Box } from "@chakra-ui/react";
-import { Card } from "./components/Card";
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppContextProvider } from "./components/AppContext";
 import { Layout } from "./components/Layout";
+import { Conta } from "./components/pages/Conta";
+import { ContaInfo } from "./components/pages/ContaInfo";
+import { Home } from "./components/pages/Home";
 
 function App() {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Box>
-          <Card />
-        </Box>
-      </Layout>
-    </ChakraProvider>
+    <BrowserRouter>
+      <AppContextProvider>
+        <ChakraProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/conta/:id" element={<Conta />} />
+              <Route path="/infoconta" element={<ContaInfo />} />
+            </Routes>
+          </Layout>
+        </ChakraProvider>
+      </AppContextProvider>
+    </BrowserRouter>
   );
 }
 
